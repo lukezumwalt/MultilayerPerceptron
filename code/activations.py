5257232262d72f5cc161d1fa98898a92a1192c70
Luke <luzu8839@colorado.edu>
2025-02-08 22:38:02 -0700

    Defines ReLU and Leaky ReLU class activation functions

--- code/activations.py
+++ code/activations.py
@@ -1,17 +1,46 @@
 import torch
 
 class ReLU():
-    #Complete this class
-    def forward(x: torch.tensor) -> torch.tensor:
-        #implement ReLU(x) here
+    '''
+    ReLU (Rectified Linear Unit) Function Class
+    Converts any input signal to a nonlinear output.
+    Any negative inputs are zeroed out.
+    '''
+    def forward(self, x: torch.tensor) -> torch.tensor:
+        '''
+        ReLU(x) = if x>0 return x, else 0.0
+        '''
+        return torch.where(x > 0, x, 0.0)
     
-    def backward(delta: torch.tensor, x: torch.tensor) -> torch.tensor:
-        #implement delta * ReLU'(x) here
+    def backward(self, delta: torch.tensor, x: torch.tensor) -> torch.tensor:
+        '''
+        Derivative of ReLU is 1 if x>0, else 0.
+        We multiply 'delta' by that mask.
+        '''
+        gradient_mask = torch.where(x > 0,                  # if x > 0
+                                    torch.ones_like(x),     # then x_i = 1.0
+                                    torch.zeros_like(x))    # else x_i = 0.0
+        return delta * gradient_mask
+
 
 class LeakyReLU():
-    #Complete this class
-    def forward(x: torch.tensor) -> torch.tensor:
-        #implement LeakyReLU(x) here
+    '''
+    Leaky ReLU (Rectified Linear Unit) Function Class
+    Leaky implies it allows a small percentage of failed inputs through.
+    This model is designed at a rate of 10% negative leak.
+    '''
+    def forward(self, x: torch.tensor) -> torch.tensor:
+        '''
+        LeakyReLU(x) = if x>0 return x, else 0.1*x
+        '''
+        return torch.where(x > 0, x, 0.1*x)
     
-    def backward(delta: torch.tensor, x: torch.tensor) -> torch.tensor:
-        #implement delta * LeakyReLU'(x) here
+    def backward(self, delta: torch.tensor, x: torch.tensor) -> torch.tensor:
+        '''
+        Derivative of LeakyReLU is 1 if x>0, else 0.1.
+        We multiply 'delta' by that factor.
+        '''
+        gradient_mask = torch.where(x > 0,                  # if x > 0
+                                    torch.ones_like(x),     # then x_i = 1.0
+                                    0.1*torch.ones_like(x)) # else x_i = 0.1
+        return delta * gradient_mask
